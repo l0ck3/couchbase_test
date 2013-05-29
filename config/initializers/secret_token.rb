3f012b3d7d46282a5d3cd9575f6c0c54add7ccde
l0ck3 <yann@wengee.co>
2013-05-29 15:30:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlertiTest::Application.config.secret_token = 'f7c633f57f8d30321fee11b3b9c20c1b0edad1cea4a3885996d52573284edf914b69bb264d2dd333a6ffd8aade3e15a0a4d63a61ae0f9421f1b362f347952245'
